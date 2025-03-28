b02038dad02d35919a48758c722c52ce8ec664f9
velchekm <velchekm@gmail.com>
2025-03-27 20:38:45 -0500

    Update my-build.js

--- build/my-build.js
+++ build/my-build.js
@@ -1,13 +1,29 @@
 module.exports = {
-    extends: "builds/base.js",
-    include: "builds/features/manualBedMesh.js",
+    board_env: "STM32F103RE_creality",
+    active: true,
+    min_version: "2.1.2",
     meta: {
-        stable_name: "ender_3_4.2.2-{{marlin_version}}-manual_mesh_7x7-{{uid}}",
-        nightly_name: "ender_3_4.2.2-{{current_date}}-manual_mesh_7x7-{{uid}}"
+        stable_name: "ender_3_4.2.7-{{marlin_version}}-{{uid}}",
+        nightly_name: "ender_3_4.2.7-{{current_date}}-{{uid}}"
+    },
+    based_on: {
+        repo: "https://github.com/MarlinFirmware/Configurations/",
+        path: "/config/examples/Creality/Ender-3/CrealityV422/",
+        stable_branch: "release-{{marlin_version}}",
+        nightly_branch: "bugfix-2.1.x"
     },
     configuration: {
         enable: [
-            ["GRID_MAX_POINTS_X", 7]
-        ]
+            //standard leveling menu helper
+            ["GRID_MAX_POINTS_X",7
+        ],
+        disable: []
+    },
+    configuration_adv: {
+        enable: [
+            //octoprint
+            "HOST_ACTION_COMMANDS"
+        ],
+        disable: []
     }
 };
